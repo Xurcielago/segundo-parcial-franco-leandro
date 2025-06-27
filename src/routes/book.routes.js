eab6601eed816cc270a7630f2238b80bec3c42c5
Xurcielago <therouses00@gmail.com>
2025-06-27 10:47:41 -0300

    Inicializado desarrollo del archivo book.routes.js con la estructura que se utilizará para las rutas

---
+++ src/routes/book.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+const routerDB = Router();
+//import {} from ../controllers/book.controllers.js
+
+routerDB.get("/books", )
+routerDB.get("/books:id", )
+routerDB.post("/books", )
+routerDB.post("/books:id",)
+routerDB.delete("/books:id",)
+
+export default routerDB
