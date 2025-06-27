b7fb5b06914cf88d1988fc3c2bf7fa9768040149
Xurcielago <therouses00@gmail.com>
2025-06-27 10:40:21 -0300

    Desarrollado archivo book.model.js, creada estructura de los objeto (book), exportados para su uso

---
+++ src/models/book.model.js
@@ -0,0 +1,12 @@
+import { sequelize } from "../config/database";
+import { DataTypes } from "sequelize";
+
+const bookModel = sequelize.define("book", {
+    title: {type: DataTypes.STRING(50), allowNull: false},
+    author: {type: DataTypes.STRING(50), allowNull: false},
+    pages: {type: DataTypes.INTEGER(50), allowNull: false},
+    genre: {type: DataTypes.STRING(30), allowNull:false},
+    description: {type: DataTypes.STRING(250)}
+})
+
+export default bookModel
