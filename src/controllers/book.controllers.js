252386e8da35c661e703f443385a121685b5cefc
Xurcielago <therouses00@gmail.com>
2025-06-27 12:42:25 -0300

    Arreglado status en el controlador post de creación, ahora muestra el registro en la consola al crearlo. Desarrollado metodo Put para actualizar registros book ya creados

--- src/controllers/book.controllers.js
+++ src/controllers/book.controllers.js
@@ -32,8 +32,8 @@ export const createBook = async (req, res) => {
             return res.status(400).json({ message: "Error: Campo genre no puede estar vacío" })
         }
 
-        const char = await bookModel.create(req.body)
-        res.status(201).json("Libro creado correctamente")
+        const bookCreated = await bookModel.create(req.body)
+        res.status(201).json(bookCreated)
     } catch (err) {
         res.status(500).json({ message: 'Error del lado interno del servidor: ', error: err.message })
     }
@@ -82,3 +82,50 @@ export const deleteBook = async (req, res) => {
         res.status(500).json({ message: 'Error del lado interno del servidor: ', error: err.message })
     }
 }
+
+//PUT /api/books/:id: actualizar un libro existente (con validaciones)
+export const updateBook = async (req, res) => {
+    const { id } = req.params;
+    let {title, author, pages, genre} = req.body;
+    
+    //Validaciones para "title"
+    if (title.trim() === '') {
+        return res.status(400).json({ message: "Error: Campo título no puede estar vacío" })
+    }
+
+    //Validaciones para "author"
+    if (author.trim() === '') {
+        return res.status(400).json({ message: "Error: Campo título no puede estar vacío" })
+    }
+
+    //Validaciones para "pages"
+    if (typeof pages !== 'number') {
+        return res.status(400).json({ message: "Error: El número de páginas debe ser un número entero" })
+    }
+    if (pages < 0) {
+        return res.status(400).json({ message: "Error: El número de páginas no debe ser negativo" })
+    }
+
+    //Validaciones para "genre"
+    if (genre.trim() === '') {
+        return res.status(400).json({ message: "Error: Campo genre no puede estar vacío" })
+    }
+    try {
+        const findBook = await bookModel.findByPk(id);
+
+        //Validaciones para "title" (La condición es que el título del libro ya exista pero que no sea el que se quiere modificar)
+        let tituloUnico = await bookModel.findOne({ where: { title } })
+        if ((tituloUnico)) {
+            return res.status(400).json({ message: "Error: Este libro ya se encuentra en la base de datos" })
+        }
+
+        if (findBook) {
+            await findBook.update({title, author, pages, genre}, {where: {id}});
+            res.status(200).json(findBook);
+        } else {
+            res.status(404).json({ error: 'El libro que se intenta actualizar no existe' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Error del lado interno del servidor: ', error: err.message })
+    }
+};
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 const routerDB = Router();
-import { createBook, deleteBook, listALLbook, listBookById } from "../controllers/book.controllers.js";
+import { createBook, deleteBook, listALLbook, listBookById, updateBook } from "../controllers/book.controllers.js";
 
 routerDB.get("/books", listALLbook)
 routerDB.get("/books/:id", listBookById)
 routerDB.post("/books", createBook )
-routerDB.post("/books/:id",)
+routerDB.put("/books/:id", updateBook)
 routerDB.delete("/books/:id", deleteBook)
 
 export default routerDB;
